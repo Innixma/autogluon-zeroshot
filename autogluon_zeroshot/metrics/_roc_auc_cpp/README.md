07c34133d85d2a9ffd8ddf1d0e8bb7fa457f7adf
Garrett Idler <gidler@amazon.com>
2023-05-15 12:38:41 -0600

    Implement ROC AUC based on radix sort

--- autogluon_zeroshot/metrics/_roc_auc_cpp/README.md
+++ autogluon_zeroshot/metrics/_roc_auc_cpp/README.md
@@ -1,7 +1,4 @@
-This C++ implementation of ROC_AUC is based on the code from https://github.com/diditforlulz273/fastauc,
-with some additional optimizations.
-
-You can find the original MIT license included within this folder (`LICENSE.md`).
+This is a custom version of roc_auc in C++ (ab)using radix sort and the memory layout of floats in the range of 1.0-2.0.
 
 ## Compile
 
--- autogluon_zeroshot/metrics/_roc_auc_cpp/cpp_auc.cpp
+++ autogluon_zeroshot/metrics/_roc_auc_cpp/cpp_auc.cpp
@@ -1,66 +1,90 @@
+#include <array>
 #include <vector>
-#include <iostream>
-#include <algorithm>
-#include <tuple>
-#include <type_traits>
 
 
-// Code based on https://github.com/diditforlulz273/fastauc , which is under MIT license
+double radix_roc_auc(bool* y_true, float* y_pred, size_t len) {
 
-// Fill the zipped vector with pairs consisting of the
-// corresponding elements of a and b. (This assumes
-// that the vectors have equal length)
-template <typename tuple_type> void zip(
-    const bool* a,
-    const float* b,
-    const size_t len,
-    std::vector<tuple_type> &zipped)
-    {
-        for(size_t i=0; i<len; ++i)
-        {
-            zipped.push_back(std::make_tuple(a[i], b[i]));
-        }
-    }
+  std::vector<unsigned long> storage1(len);
+  std::vector<unsigned long> storage2(len);
 
-double trapezoid_area(double x1, double x2, double y1, double y2) {
-  double dx = x2 - x1;
-  double dy = y2 - y1;
-  return dx * y1 + dy * dx / 2.0;
-}
+  std::array<unsigned long, 256 * 3> histograms = {0};
 
-template <typename tuple_type> double auc_kernel(float* ts, bool* st, size_t len) {
-  // sort the data
-  // Zip the vectors together
-  std::vector<tuple_type> zipped;
-  zipped.reserve(len);
-  zip<tuple_type>(st, ts, len, zipped);
-
-  // Sort the vector of pairs
-  std::sort(std::begin(zipped), std::end(zipped),
-    [&](const auto& a, const auto& b)
-    {
-        return std::get<1>(a) > std::get<1>(b);
-    });
-
-  double fps = 0;
-  double tps = 0;
-  double last_counted_fps = 0;
-  double last_counted_tps = 0;
-  double auc = 0;
-  for (size_t i=0; i < zipped.size(); ++i) {
-    tps += std::get<0>(zipped[i]);
-    fps += (1 - std::get<0>(zipped[i]));
-    if ((i == zipped.size() - 1) || (std::get<1>(zipped[i+1]) != std::get<1>(zipped[i]))) {
-        auc += trapezoid_area(last_counted_fps, fps, last_counted_tps, tps);
-        last_counted_fps = fps;
-        last_counted_tps = tps;
-    }
+  // Add 1 to all floats so they are in range [1.0,2.0) to exploit IEEE-754 float memory layout
+  for (size_t i = 0; i < len; ++i) {
+    y_pred[i] += 1.0;
   }
-  return auc / (tps * fps);
+
+  // Interpret float32 as uint32. Put bool in 8 MSB and float mantissa in 23 LBS's of the uint32. Move to storage vector.
+  for (size_t i = 0; i < len; ++i) {
+    unsigned long int_entry = *reinterpret_cast<unsigned long*>(&y_pred[i]);
+    storage1[i] = (static_cast<unsigned long>(y_true[i]) << 24) | (int_entry & 0x7FFFFF);
+  }
+
+  // Fill 3 histograms for 8-bit radix's
+  for (size_t i = 0; i < len; ++i) {
+    histograms[storage1[i] & 0xFF]++;
+    histograms[256 + (storage1[i] >> 8 & 0xFF)]++;
+    histograms[512 + (storage1[i] >> 16 & 0xFF)]++;
+  }
+
+  // Add incremental counts to each histogram entry
+  unsigned long radix0_cnt = 0;
+  unsigned long radix1_cnt = 0;
+  unsigned long radix2_cnt = 0;
+  unsigned long temp;
+
+  for (size_t i = 0; i < 256; ++i) {
+    temp = histograms[i] + radix0_cnt;
+    histograms[i] = radix0_cnt - 1;
+    radix0_cnt = temp;
+
+    temp = histograms[i + 256] + radix1_cnt;
+    histograms[i + 256] = radix1_cnt - 1;
+    radix1_cnt = temp;
+
+    temp = histograms[i + 512] + radix2_cnt;
+    histograms[i + 512] = radix2_cnt - 1;
+    radix2_cnt = temp;
+  }
+
+  // Sort radix0 (least significant bit)
+  for (unsigned long i = 0; i < len; ++i) {
+    unsigned long entry = storage1[i];
+    storage2[++histograms[entry & 0xFF]] = entry;
+  }
+
+  // Sort radix1
+  for (unsigned long i = 0; i < len; ++i) {
+    unsigned long entry = storage2[i];
+    storage1[++histograms[256 + (entry >> 8 & 0xFF)]] = entry;
+  }
+
+  // Sort radix2 (most significant bit)
+  for (unsigned long i = 0; i < len; ++i) {
+    unsigned long entry = storage1[i];
+    storage2[++histograms[512 + (entry >> 16 & 0xFF)]] = entry;
+  }
+
+  // Perform binary non-weighted roc_auc calulation
+  unsigned long true_cnt = 0;
+  unsigned long false_cnt = 0;
+  unsigned long auc = 0;
+
+  for (unsigned long i = 0; i < len; ++i) {
+    unsigned long entry = storage2[len - 1 - i];
+    bool label = entry >> 24 & 0xFF;
+
+    true_cnt += label;
+    false_cnt += !label;
+    auc += !label * true_cnt;
+  }
+
+  return (double) auc / (true_cnt * false_cnt);
 }
 
 extern "C" {
     double cpp_auc_ext(float* ts, bool* st, size_t len) {
-        return auc_kernel<std::tuple<bool, float>>(ts, st, len);
+        return radix_roc_auc(st, ts, len);
     }
 }
+
