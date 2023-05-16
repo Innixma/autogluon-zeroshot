de496e0afd5975e8390ae83c01e72f742706308a
Garrett Idler <gidler@amazon.com>
2023-05-15 23:41:34 -0600

    Make int types more specific

--- autogluon_zeroshot/metrics/_roc_auc_cpp/cpp_auc.cpp
+++ autogluon_zeroshot/metrics/_roc_auc_cpp/cpp_auc.cpp
@@ -4,10 +4,10 @@
 
 double radix_roc_auc(bool* y_true, float* y_pred, size_t len) {
 
-  std::vector<unsigned long> storage1(len);
-  std::vector<unsigned long> storage2(len);
+  std::vector<uint32_t> storage1(len);
+  std::vector<uint32_t> storage2(len);
 
-  std::array<unsigned long, 256 * 3> histograms = {0};
+  std::array<uint32_t, 256 * 3> histograms = {0};
 
   // Add 1 to all floats so they are in range [1.0,2.0) to exploit IEEE-754 float memory layout
   for (size_t i = 0; i < len; ++i) {
@@ -16,8 +16,8 @@ double radix_roc_auc(bool* y_true, float* y_pred, size_t len) {
 
   // Interpret float32 as uint32. Put bool in 8 MSB and float mantissa in 23 LBS's of the uint32. Move to storage vector.
   for (size_t i = 0; i < len; ++i) {
-    unsigned long int_entry = *reinterpret_cast<unsigned long*>(&y_pred[i]);
-    storage1[i] = (static_cast<unsigned long>(y_true[i]) << 24) | (int_entry & 0x7FFFFF);
+    uint32_t int_entry = *reinterpret_cast<uint32_t*>(&y_pred[i]);
+    storage1[i] = (static_cast<uint32_t>(y_true[i]) << 24) | (int_entry & 0x7FFFFF);
   }
 
   // Fill 3 histograms for 8-bit radix's
@@ -28,10 +28,10 @@ double radix_roc_auc(bool* y_true, float* y_pred, size_t len) {
   }
 
   // Add incremental counts to each histogram entry
-  unsigned long radix0_cnt = 0;
-  unsigned long radix1_cnt = 0;
-  unsigned long radix2_cnt = 0;
-  unsigned long temp;
+  uint32_t radix0_cnt = 0;
+  uint32_t radix1_cnt = 0;
+  uint32_t radix2_cnt = 0;
+  uint32_t temp;
 
   for (size_t i = 0; i < 256; ++i) {
     temp = histograms[i] + radix0_cnt;
@@ -48,30 +48,30 @@ double radix_roc_auc(bool* y_true, float* y_pred, size_t len) {
   }
 
   // Sort radix0 (least significant bit)
-  for (unsigned long i = 0; i < len; ++i) {
-    unsigned long entry = storage1[i];
+  for (size_t i = 0; i < len; ++i) {
+    uint32_t entry = storage1[i];
     storage2[++histograms[entry & 0xFF]] = entry;
   }
 
   // Sort radix1
-  for (unsigned long i = 0; i < len; ++i) {
-    unsigned long entry = storage2[i];
+  for (size_t i = 0; i < len; ++i) {
+    uint32_t entry = storage2[i];
     storage1[++histograms[256 + (entry >> 8 & 0xFF)]] = entry;
   }
 
   // Sort radix2 (most significant bit)
-  for (unsigned long i = 0; i < len; ++i) {
-    unsigned long entry = storage1[i];
+  for (size_t i = 0; i < len; ++i) {
+    uint32_t entry = storage1[i];
     storage2[++histograms[512 + (entry >> 16 & 0xFF)]] = entry;
   }
 
   // Perform binary non-weighted roc_auc calulation
-  unsigned long true_cnt = 0;
-  unsigned long false_cnt = 0;
-  unsigned long auc = 0;
+  uint64_t true_cnt = 0;
+  uint64_t false_cnt = 0;
+  uint64_t auc = 0;
 
-  for (unsigned long i = 0; i < len; ++i) {
-    unsigned long entry = storage2[len - 1 - i];
+  for (size_t i = 0; i < len; ++i) {
+    uint32_t entry = storage2[len - 1 - i];
     bool label = entry >> 24 & 0xFF;
 
     true_cnt += label;
